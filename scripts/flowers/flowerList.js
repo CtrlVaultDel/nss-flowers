8f0c405c8d8068114c1a09d8d67472a1a4da367e
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-01-11 11:46:38 -0600

    Moved distribution function called from flowers folder to retailers

--- scripts/flowers/flowerList.js
+++ scripts/flowers/flowerList.js
@@ -1,6 +1,5 @@
 // Imports
 import { getFlowers, useFlowers } from "./flowerProvider.js";
-import { getDistributors, useDistributors } from "../distributors/distributorProvider.js";
 import { flowerHTML } from "./flower.js";
 
 // Selectors
@@ -8,14 +7,11 @@ const flowerContainer = document.querySelector('.flowerContainer');
 
 // Local Variables
 let flowers = [];
-let distributors = [];
 
 export const flowerList = () => {
     getFlowers()
-    .then(getDistributors)
     .then(() => {
         flowers = useFlowers();
-        distributors = useDistributors();
         flowerContainer.innerHTML = flowers.map(flower => flowerHTML(flower)).join("");
     });
 };
--- scripts/retailers/retailerList.js
+++ scripts/retailers/retailerList.js
@@ -1,5 +1,6 @@
 // Imports
 import { getRetailers, useRetailers } from "./retailerProvider.js";
+import { getDistributors, useDistributors } from "../distributors/distributorProvider.js";
 import { retailerHTML } from "./retailer.js";
 
 // Selectors
@@ -7,11 +8,15 @@ const retailerContainer = document.querySelector('.retailerContainer');
 
 // Local Variables
 let retailers = [];
+let distributors = [];
 
 export const retailerList = () => {
     getRetailers()
+    .then(getDistributors)
     .then(() => {
         retailers = useRetailers();
+        distributors = useDistributors();
+        
         retailerContainer.innerHTML = retailers.map(retailer => retailerHTML(retailer)).join("");
     });
 };
